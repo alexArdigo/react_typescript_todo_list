b26d7831c731bdbe377df11b0957432abc2331c4
alexArdigo <alexaardigo@gmail.com>
2024-01-31 22:06:19 +0000

    Refactor App.tsx to implement Todo List functionality and update associated styles

---
+++ src/components/InputField.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import "./styles.css";
+import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
+
+type Props = {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAddTodo: (e: React.FormEvent) => void;
+};
+
+const InputField = ({ todo, setTodo, handleAddTodo }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  
+  return (
+    <form 
+      className="input" 
+      onSubmit={(e) => {
+        handleAddTodo(e)
+        inputRef.current?.blur()
+      }}
+      >
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input-box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input-submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
